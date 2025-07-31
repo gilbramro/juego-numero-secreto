459ba69662e4cf20ef08e00c313ec371ecf18c42
Gilbramro <gilramrom@gmail.com>
2025-07-30 21:26:41 -0500

    Se realiza declaracion de variable tipo array, captura de valor, validación de entrada, método push y limpieza campo de entrada.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaración de Variable tipo array
+let amigos = [];
+
+// Capturar Valor de entrada
+function agregarAmigo() {
+    let ingresarAmigo = document.getElementById('amigo').value;
+
+// Validación de entrada y método push  
+    if (ingresarAmigo === "") {
+        alert("Por favor ingrese un nombre.");
+    } else {
+        amigos.push(ingresarAmigo);
+        console.log(amigos);
+
+        limpiarCampoEntrada();
+    }
+
+}
+// Limpiar campo de entrada
+function limpiarCampoEntrada() {
+    document.querySelector('#amigo').value = '';
+}
